90cf20b2598ea318ca50a655e8bc53b371b5ac0d
Jeremy Roberts <jaroberts@ksu.edu>
2020-09-11 17:48:42 -0500

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,1 +1,4 @@
-echo "32 F = 0 C"
+# simple solution with bash arithmetic
+F=$1 # this is the first argument and is my temperature in fehrenheit
+C=$(( (F-32)*5/9 )) # need (( ... )) for bash arithmetic; see TLCL page 71 ("arithmetic expansion")
+echo "$F F = $C C"
